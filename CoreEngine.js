bbf921c1a48f308d3edf29e72f73071159201894
Baran Hunter <aoodiakdiro0001@gmail.com>
2026-08-29 19:42:04 +0300

    Use lowercase addresses to fix checksum error

--- CoreEngine.js
+++ CoreEngine.js
@@ -6,15 +6,15 @@ const TELEGRAM_CHAT_ID = "589920599";
 
 const provider = new ethers.JsonRpcProvider(RPC_URL);
 
-const ROUTER_TRADER_JOE = ethers.getAddress("0x60aE616a2155Ee3d9A68541Ba4544862310933d4");
-const ROUTER_PANGOLIN = ethers.getAddress("0xE54Ca86531e17ef3616d22Ca28b0D458b6C81616");
+const ROUTER_TRADER_JOE = "0x60ae616a2155ee3d9a68541ba4544862310933d4";
+const ROUTER_PANGOLIN = "0xe54ca86531e17ef3616d22ca28b0d458b6c81616";
 
 const ROUTER_ABI = [
     "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)"
 ];
 
-const WAVAX = ethers.getAddress("0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7");
-const USDC = ethers.getAddress("0xB97EF9Ef8734C71904D8002F8b6bc66Dd9c48a6E");
+const WAVAX = "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7";
+const USDC = "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e";
 
 const TRADE_AMOUNT = ethers.parseUnits("0.5", 18);
 
